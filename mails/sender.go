0a7467cdcfc9ab9f08d613bb040485d789691bf0
Mathieu Tortuyaux <mathieu.tortuyaux@optelgroup.com>
2018-01-07 15:21:12 +0100

    style(mail/sender): run go formatting tool

--- mails/sender.go
+++ mails/sender.go
@@ -8,14 +8,14 @@ import (
 
 /* This file has not to be tested, `gomail` has his own test series */
 
-func SendMail(d *gomail.Dialer, m *gomail.Message)  bool{
+func SendMail(d *gomail.Dialer, m *gomail.Message) bool {
 	/*
 		Send a message with smtp server
 		parameter: <gomail.Dialer> A struct to handler communication with smtp
 		parameter: <gomail.Message> Message to send
 		return: <bool> Return true if message has been sent
 	*/
-	if err := d.DialAndSend(m); err != nil{
+	if err := d.DialAndSend(m); err != nil {
 		fmt.Println(err)
 		return false
 	}
