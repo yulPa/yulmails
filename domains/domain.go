61c21a8b07769f173a7eb49b95239fed3ceb06e2
Mathieu Tortuyaux <mathieu.tortuyaux@optelgroup.com>
2018-01-07 19:13:12 +0100

    feat(domains): create authorized pool from json

--- domains/domain.go
+++ domains/domain.go
@@ -1,24 +1,29 @@
 package domains
 
-type Environment struct{
-  /*
-    A environment is a list of domains
-  */
-  Name string
-  domains []string
+import "encoding/json"
+
+type Environment struct {
+	Name    string   `json:"name"`
+	domains []string `json:"domains"`
 }
 
-type Allowed struct{
-  Env Environment
+type AuthorizedPool struct {
+	Env []Environment `json:"environments"`
 }
 
-func NewAllowed(env Environment) *Allowed{
-  /*
-    This function create an Allowed with a given set of environment
-    parameter: <environment> A given set of environment
-    return: <Allowed> A kind of list of authorized domains
-  */
-  return &Allowed{
-    Env: env,
-  }
+func NewAuthorizedPool(env []Environment) *AuthorizedPool {
+	/*
+	   This function create an AuthorizedPool with a given set of environments
+	   parameter: <environment> A given set of environment
+	   return: <AuthorizedPool> A kind of list of authorized domains
+	*/
+	return &AuthorizedPool{
+		Env: env,
+	}
+}
+
+func CreateANewAuthorizedPoolFromJson(data []byte) *AuthorizedPool {
+	var AuthorizedPool AuthorizedPool
+	json.Unmarshal(data, &AuthorizedPool)
+	return &AuthorizedPool
 }
--- domains/domain_test.go
+++ domains/domain_test.go
@@ -5,15 +5,52 @@ import (
 	"testing"
 )
 
-func TestCreateANewAllowed(t *testing.T) {
-  e := Environment{
-    Name: "environment1",
-    domains: []string{
-      "domain1",
-      "domain2",
-      "domain3",
-    },
-  }
-  a := NewAllowed(e)
-  assert.Equal(t, "domain2", a.Env.domains[1])
+func TestCreateANewAuthorizedPool(t *testing.T) {
+	e := []Environment{
+		Environment{
+			Name: "environment1",
+			domains: []string{
+				"domain1",
+				"domain2",
+				"domain3",
+			},
+		},
+		Environment{
+			Name: "environment2",
+			domains: []string{
+				"domain1",
+				"domain2",
+				"domain3",
+			},
+		},
+	}
+	a := NewAuthorizedPool(e)
+	assert.Equal(t, "domain1", a.Env[1].domains[0])
+}
+
+func TestCreateAAuthorizedPoolFromJson(t *testing.T) {
+	_data := []byte(`
+    {
+      "environments": [
+        {
+          "name": "zimbra",
+          "domains": [
+            "zimbra1.com",
+            "zimbra2.com",
+            "zimbraN.com"
+          ]
+        },
+        {
+          "name": "http",
+          "domains": [
+            "http1.com",
+            "http2.com",
+            "httpN.com"
+          ]
+        }
+      ]
+    }
+  `)
+	a := CreateANewAuthorizedPoolFromJson(_data)
+	assert.Equal(t, a.Env[1].Name, "http")
 }
