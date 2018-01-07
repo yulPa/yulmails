057db587c4f5e2306a3ab6a1b183ba9429662caf
Mathieu Tortuyaux <mathieu.tortuyaux@optelgroup.com>
2018-01-07 22:07:58 +0100

    feat(main): start api server

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
-import "github.com/yulPa/check_mails/logger"
+import (
+	"github.com/yulPa/check_mails/api"
+)
 
 func main() {
-	log := logger.GetLogger()
-	log.Println("Hello, world")
+	api.Start()
 }
