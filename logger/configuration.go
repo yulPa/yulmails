9c9e3a77fa4490bad9b7838d40137d9e4cc83a8b
Mathieu Tortuyaux <mathieu.tortuyaux@optelgroup.com>
2018-01-07 16:23:47 +0100

    style(logger): run go fmt tool

--- logger/configuration.go
+++ logger/configuration.go
@@ -1,26 +1,26 @@
 package logger
 
 import (
-  "os"
+	"os"
 
-  "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 )
 
-func GetLogger() *logrus.Logger{
-  /*
-    This method create a new custom logger
-    return: <logrus> A custom logrus logger
-  */
-  var log = logrus.New()
-  // Set format to JSON can be useful if hooks are used
-  log.Formatter = new(logrus.JSONFormatter)
-  log.Level = logrus.DebugLevel
+func GetLogger() *logrus.Logger {
+	/*
+	   This method create a new custom logger
+	   return: <logrus> A custom logrus logger
+	*/
+	var log = logrus.New()
+	// Set format to JSON can be useful if hooks are used
+	log.Formatter = new(logrus.JSONFormatter)
+	log.Level = logrus.DebugLevel
 
-  file, err := os.OpenFile("/var/log/check_mails.log", os.O_CREATE|os.O_WRONLY, 0666)
-  if err == nil {
-  	log.Out = file
-  } else {
-    panic("Log file is not reachable, please assert that /var/log is created and accessible")
-  }
-  return log
+	file, err := os.OpenFile("/var/log/check_mails.log", os.O_CREATE|os.O_WRONLY, 0666)
+	if err == nil {
+		log.Out = file
+	} else {
+		panic("Log file is not reachable, please assert that /var/log is created and accessible")
+	}
+	return log
 }
