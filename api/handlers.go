06fcd6f70db29029fba89d18c93c702ed1c501d0
Mathieu Tortuyaux <mathieu.tortuyaux@optelgroup.com>
2018-01-08 13:27:08 +0100

    feat(api/handlers): add a route to get entitys

--- api/handlers.go
+++ api/handlers.go
@@ -1,7 +1,7 @@
 package api
 
 import (
-	"github.com/yulPa/yulmails/domains"
+	"github.com/yulPa/yulmails/entity"
 	"github.com/yulPa/yulmails/logger"
 
 	"io/ioutil"
@@ -10,14 +10,38 @@ import (
 
 var log = logger.GetLogger()
 
-func CreateAuthorizedPool(w http.ResponseWriter, r *http.Request) {
+func CreateEntity(w http.ResponseWriter, r *http.Request) {
 	/*
-	   Create an authorized Pool from HTTP request
+	   Create an entity Pool from HTTP request
 	*/
 
 	b, _ := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
 
-	pool := domains.CreateANewAuthorizedPoolFromJson(b)
-	log.Infoln(pool)
+	entity := entity.NewEntity(b)
+	// TODO: Send entity into MONGO DB
+	log.Infoln(entity)
+}
+
+func GetEntity(w http.ResponseWriter, r *http.Request) {
+	/*
+		Return a JSON list of present entitys
+	*/
+
+	// TODO: Fetch entitys from DB
+	e := []byte(`
+		{
+		  "name": "An entity",
+		  "abuse": "abuse@domain.tld",
+		  "conservation":{
+		    "sent": 5,
+		    "unsent": 2,
+		    "keep": true
+		  }
+		}
+		`)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(e)
 }
