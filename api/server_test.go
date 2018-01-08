5dd68a33528a88244325b49fd3617eae238bba01
Mathieu Tortuyaux <mathieu.tortuyaux@optelgroup.com>
2018-01-08 13:27:50 +0100

    style(server): run go fmt tool

--- api/server_test.go
+++ api/server_test.go
@@ -1,28 +1,28 @@
 package api
 
 import (
-  "net/http/httptest"
-  "net/http"
-  "testing"
-  "fmt"
-  "io/ioutil"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
-  "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 
-  "github.com/yulPa/yulmails/entity"
+	"github.com/yulPa/yulmails/entity"
 )
 
 var router = GetRouterV1()
 
-func TestGetEntitys(t *testing.T)  {
+func TestGetEntitys(t *testing.T) {
 
-  ts := httptest.NewServer(router)
+	ts := httptest.NewServer(router)
 	defer ts.Close()
 
 	res, _ := http.Get(fmt.Sprintf("%s/%s", ts.URL, "api/v1/entity"))
 	body, _ := ioutil.ReadAll(res.Body)
 
-  entity := entity.NewEntity(body)
+	entity := entity.NewEntity(body)
 
-  assert.Equal(t, "abuse@domain.tld", entity.Abuse)
+	assert.Equal(t, "abuse@domain.tld", entity.Abuse)
 }
