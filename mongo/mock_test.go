25f496f5e763ad515b0ca7b02e1f35a3a3edbc34
Mathieu Tortuyaux <mathieu.tortuyaux@optelgroup.com>
2018-01-09 22:48:02 +0100

    test(mongo/mock): add test in order to increase cov

--- mongo/mock_test.go
+++ mongo/mock_test.go
@@ -36,3 +36,40 @@ func TestCopyASession(t *testing.T) {
 	copyConn := conn.Copy()
 	assert.IsType(t, copyConn, MockSession{})
 }
+
+func TestInsertSomething(t *testing.T)  {
+
+	type Test struct {Yolo string}
+
+	conn := NewMockSession()
+	db := conn.DB("a_database")
+	col := db.C("a_collection")
+	err := col.Insert(&Test{Yolo: "hello"})
+	assert.Nil(t, err)
+}
+
+func TestFindSomething(t *testing.T)  {
+
+	type Test struct {Yolo string}
+
+	conn := NewMockSession()
+	db := conn.DB("a_database")
+	col := db.C("a_collection")
+	res := col.Find(&Test{Yolo: "hello"})
+	assert.IsType(t, res, MockQuery{})
+}
+
+func TestFetchSomething(t *testing.T)  {
+
+	type Test struct {Yolo string}
+	var test Test
+
+	conn := NewMockSession()
+	db := conn.DB("a_database")
+	col := db.C("a_collection")
+	res := col.Find(&Test{Yolo: "hello"}).All(test)
+
+	assert.Nil(t, res)
+	assert.IsType(t, test, Test{})
+
+}
