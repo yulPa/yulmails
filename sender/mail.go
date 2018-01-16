088d20afdde3df3f51da7e3f963edfaa5d406c6d
Mathieu Tortuyaux <mathieu.tortuyaux@optelgroup.com>
2018-01-16 10:54:23 +0100

    fix(sender): store hash and timestamp as string

--- sender/mail.go
+++ sender/mail.go
@@ -3,19 +3,20 @@ package sender
 import (
 	"crypto/sha256"
 	"encoding/json"
+	"strconv"
 	"time"
 
 	"fmt"
 )
 
 type Mail struct {
-	From        string            `json:"from"`
-	To          []string          `json:"to"`
-	Object      string            `json:"object"`
-	Content     string            `json:"content"`
-	Hash        [sha256.Size]byte `json:"hash"`
-	Timestamp   int64             `json:"timestamp"`
-	Environment string            `json:"environment,omitempty"`
+	From        string   `json:"from"`
+	To          []string `json:"to"`
+	Object      string   `json:"object"`
+	Content     string   `json:"content"`
+	Hash        string   `json:"hash,omitempty"`
+	Timestamp   string   `json:"timestamp"`
+	Environment string   `json:"environment,omitempty"`
 }
 
 func NewMail(from string, to []string, object string, content string) *Mail {
@@ -31,8 +32,8 @@ func NewMail(from string, to []string, object string, content string) *Mail {
 		To:        to,
 		Object:    object,
 		Content:   content,
-		Hash:      sha256.Sum256([]byte(fmt.Sprintf("%s:%s:%s", from, object, content))),
-		Timestamp: time.Now().Unix(),
+		Hash:      fmt.Sprintf("%s", sha256.Sum256([]byte(fmt.Sprintf("%s:%s:%s", from, object, content)))),
+		Timestamp: strconv.Itoa(int(time.Now().Unix())),
 	}
 }
 
