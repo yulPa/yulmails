2d92d5e51207c9e52d6cd437894e60d4eac28af2
Mathieu Tortuyaux <mathieu.tortuyaux@optelgroup.com>
2018-01-24 22:24:27 +0100

    feast(main): start entrypoint

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/yulPa/yulmails/api"
+	"github.com/yulPa/yulmails/configuration"
+	"github.com/yulPa/yulmails/entrypoint"
 )
 
 func main() {
+	conf := configuration.NewConfigurationFromConfFile()
+	fmt.Println(conf)
+
+	/*
+		Start Go Subroutines
+	*/
+	go entrypoint.Run()
+
 	api.Start()
 }
