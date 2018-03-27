1e8643e132f59372c29f6ab936013bc266542d12
Mathieu Tortuyaux <mathieu.tortuyaux@optelgroup.com>
2018-03-27 21:29:20 +0200

    feat(ms): add one file per service

---
+++ api.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/yulPa/yulmails/api"
+	"github.com/yulPa/yulmails/pkg/configuration"
+	"github.com/yulPa/yulmails/pkg/logger"
+	"github.com/yulPa/yulmails/pkg/mongo"
+	"github.com/yulPa/yulmails/pkg/sender"
+	"github.com/yulPa/yulmails/pkg/entrypoint"
+)
+
+var (
+	log      = logger.GetLogger("yulmails-api")
+	certFile = flag.String("tls-crt-file", "domain.tld.crt", "A certificate file")
+	keyFile  = flag.String("tls-key-file", "domain.tld.key", "A key file")
+)
+
+func main() {
+
+	flag.Parse()
+
+
+	var workdb = mongo.NewSession("mongodb://workdb:27017")
+	var archivingdb = mongo.NewSession("mongodb://archivingdb:27017")
+
+	log.Info(workdb)
+	log.Info(archivingdb)
+
+	defer archivingdb.Close()
+	defer workdb.Close()
+
+	err := configuration.NewConfigurationFromConfFile(archivingdb)
+
+	if err != nil {
+		log.Errorln(err)
+		panic(err)
+	}
+
+
+	/*
+		Start Go Subroutines
+	*/
+	go entrypoint.Run()
+
+	go sender.Run()
+	api.Start(*certFile, *keyFile)
+}
---
+++ compute.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/yulPa/yulmails/pkg/mongo"
+	"github.com/yulPa/yulmails/pkg/logger"
+)
+
+var (
+	log      = logger.GetLogger("yulmails-compute")
+)
+
+func main() {
+
+	var workdb = mongo.NewSession("mongodb://workdb:27017")
+	log.Infoln(workdb)
+
+
+}
