e10ac1805b2d46c2bd2fb5e3d558f8cea704748f
Mathieu Tortuyaux <mathieu.tortuyaux@optelgroup.com>
2018-03-29 12:04:41 +0200

    feat(main): add first cli command

--- main.go
+++ main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
-	"flag"
+	"fmt"
+	"github.com/spf13/cobra"
 
-	"github.com/yulPa/yulmails/api"
+	"gitub.com/yulPa/yulmails/api"
 )
 
-var (
-	certFile = flag.String("tls-crt-file", "domain.tld.crt", "A certificate file")
-	keyFile  = flag.String("tls-key-file", "domain.tld.key", "A key file")
-)
+var certFile string
+var keyFile string
 
 func main() {
+	var cmdApi = &cobra.Command{
+		Use:   "api ",
+		Short: "Start the API configuration server",
+		Args: cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			api.Start(*certFile, *keyFile)
+		},
+	}
+	cmdApi.Flags().StringVarP(&certFile, "tls-crt-file", "", "domain.tld.crt", "A certificate file")
+	cmdApi.Flags().StringVarP(&keyFile, "tls-key-file", "", "domain.tld.key", "A key file")
 
-	flag.Parse()
-	api.Start(*certFile, *keyFile)
+
+	var rootCmd = &cobra.Command{Use: "yulmails"}
+	rootCmd.AddCommand(cmdApi, cmdToto)
+	rootCmd.Execute()
 }
