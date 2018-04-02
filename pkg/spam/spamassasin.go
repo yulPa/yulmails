b7711480433f4f3fcd2abc8fdcfe189151baf116
Mathieu Tortuyaux <mathieu.tortuyaux@optelgroup.com>
2018-04-02 16:41:33 +0200

    perf(spam): remove spam check from init

--- pkg/spam/spamassasin.go
+++ pkg/spam/spamassasin.go
@@ -1,25 +1,47 @@
-package main
+package spam
 
 import (
-  "log"
   "os/exec"
   "bytes"
+  "time"
+
+  "github.com/robfig/cron"
+
+  "github.com/yulPa/yulmails/pkg/logger"
 )
 
-func init()  {
+var log = logger.GetLogger("ym-compute")
+
+func checkSpamAssassin()  {
   /*
     This init method is called each time this package is loaded
     We check if `spamassassin` is installed on the machine
   */
-  cmd := exec.Command("which", "spamassassin")
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
-	if err != nil {
-		log.Fatal("`spamassassin` is not installed on this machine: ", err)
-	}
+
 }
 
-func main()  {
-  log.Printf("Hello, World")
+func computeMail()  {
+  log.Infoln("Compute email")
+}
+
+func Run() {
+
+  var out bytes.Buffer
+  var c = cron.New()
+
+  cmd := exec.Command("which", "spamassassin")
+  cmd.Stdout = &out
+
+  if err := cmd.Run(); err != nil {
+    log.Fatal("`spamassassin` is not installed on this machine: ", err)
+  }
+
+	c.AddFunc("@every 5s", computeMail)
+
+	log.Infoln("Compute Cron has started")
+	c.Start()
+
+	for {
+		time.Sleep(1000*time.Millisecond)
+	}
 }
