e6e18c77dcd3a232fa7d481eac446a2131c349eb
Mathieu Tortuyaux <mathieu.tortuyaux@optelgroup.com>
2018-04-16 21:51:21 +0200

    feat(compute/sa): add spamassassin check

--- pkg/spam/spamassasin.go
+++ pkg/spam/spamassasin.go
@@ -2,7 +2,9 @@ package spam
 
 import (
 	"bytes"
+	"net/mail"
 	"os/exec"
+	"strings"
 	"time"
 
 	"github.com/robfig/cron"
@@ -12,11 +14,19 @@ import (
 
 var log = logger.GetLogger("ym-compute")
 
-func checkSpamAssassin() {
+func checkSpamAssassin(content string) {
 	/*
 	   This method will call SA in order to determine if mails are spams
 	*/
+	output, _ := exec.Command("spamassassin", content).Output()
+	raw := string(output)
 
+	r := strings.NewReader(raw)
+	m, _ := mail.ReadMessage(r)
+	if strings.Split(m.Header.Get("X-Spam-Status"), ",")[0] == "Yes" {
+		log.Println("Spam")
+	}
+	log.Println("not spam")
 }
 
 func computeMail() {
