9db54a4a14193612257d6a65d45d9a5525169b19
Mathieu Tortuyaux <mathieu.tortuyaux@optelgroup.com>
2018-06-03 15:08:11 +0200

    feast(entrypoint): check if blacklist

--- pkg/entrypoint/entrypoint.go
+++ pkg/entrypoint/entrypoint.go
@@ -1,15 +1,50 @@
 package entrypoint
 
 import (
-	"bufio"
 	"log"
-	"os"
+	"net/http"
+	"net/mail"
+
+	"github.com/yulPa/yulmails/pkg/environment"
+	"github.com/yulPa/yulmails/pkg/mongo"
 )
 
+// Run method
 func Run() {
-	reader := bufio.NewScanner(os.Stdin)
-	for reader.Scan() {
-		log.Print(reader.Text())
+	http.HandleFunc("/mail", func(w http.ResponseWriter, req *http.Request) {
+		if req.Method == "POST" {
+			email, err := mail.ReadMessage(req.Body)
+			if err != nil {
+				log.Printf("unable to read message: %v", err)
+			}
+
+			log.Print(checkIfAuthorize(email.Header.Get("from")))
+		}
+	})
+	http.ListenAndServe(":8080", nil)
+}
+
+func checkIfAuthorize(from string) bool {
+	var archivingdb = mongo.NewSession("mongodb://archivingdb:27017")
+
+	db := archivingdb.DB("configuration")
+
+	env, err := db.ReadEnvironments("yulpa")
+	if err != nil {
+		log.Printf("unable to read environments: %v", err)
 	}
 
+	return contains(from, env)
+
+}
+
+func contains(s string, a []environment.Environment) bool {
+	for _, env := range a {
+		for _, el := range env.IPs {
+			if s == el {
+				return true
+			}
+		}
+	}
+	return false
 }
