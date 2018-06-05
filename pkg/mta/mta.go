f22e79c338bbb32e67564794b7a8581e4c48f966
Mathieu Tortuyaux <mathieu.tortuyaux@optelgroup.com>
2018-06-05 21:53:04 +0200

    feat(mta): add save processor

--- pkg/mta/mta.go
+++ pkg/mta/mta.go
@@ -4,18 +4,26 @@ import (
 	"log"
 	"time"
 
+	"github.com/flashmob/go-guerrilla/backends"
+
 	"github.com/flashmob/go-guerrilla"
+
+	"github.com/yulPa/yulmails/pkg/mta/processor"
 )
 
 var (
 	cfg = &guerrilla.AppConfig{
 		LogFile:      "/tmp/guerrilla.log",
 		AllowedHosts: []string{"arch.localdomain"},
+		BackendConfig: backends.BackendConfig{
+			"save_process": "YulmailsProcessor",
+		},
 	}
 	d = guerrilla.Daemon{Config: cfg}
 )
 
 func main() {
+	d.AddProcessor("YulmailsProcessor", processor.YulmailsProcessor)
 	if err := d.Start(); err != nil {
 		log.Fatalf("unable to start mta: %v", err)
 	}
