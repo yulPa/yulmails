790700e3c743e65bdc16b85115772c89972f705d
Mathieu Tortuyaux <mathieu.tortuyaux@gmail.com>
2019-06-09 20:40:46 +0200

    feat(conservation/api): check if database connection

--- services/conservation/main.go
+++ services/conservation/main.go
@@ -7,15 +7,17 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"github.com/grpc-ecosystem/grpc-gateway/utilities"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 
 	gw "gitlab.com/tortuemat/yulmails/services/conservation/v1beta1"
 )
 
-func run() error {
+func run(db *sql.DB) error {
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
@@ -26,6 +28,25 @@ func run() error {
 	if err != nil {
 		return err
 	}
+
+	pattern, err := runtime.NewPattern(1, []int{int(utilities.OpLitPush), 0}, []string{"healthz"}, "")
+	if err != nil {
+		log.Printf("unable to create pattern: %s", err)
+	}
+	mux.Handle(
+		"GET",
+		pattern,
+		func(w http.ResponseWriter, r *http.Request, _ map[string]string) {
+			ctx, cancel := context.WithTimeout(r.Context(), 1*time.Second)
+			defer cancel()
+			if err := db.PingContext(ctx); err != nil {
+				http.Error(w, fmt.Sprintf("unable to connect to db: %v", err), http.StatusFailedDependency)
+				return
+			}
+			w.WriteHeader(http.StatusOK)
+			return
+		},
+	)
 	log.Println("HTTP server started on localhost:8080")
 	return http.ListenAndServe(":8080", mux)
 }
@@ -58,7 +79,7 @@ func main() {
 
 	go server.Serve(listen)
 	log.Println("gRPC server started on localhost:9090")
-	if err := run(); err != nil {
+	if err := run(db); err != nil {
 		log.Fatal(err)
 	}
 }
