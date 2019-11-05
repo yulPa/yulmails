19d28ce384a642be31e9c38491bbf2e1b33971c4
Mathieu Tortuyaux <mathieu.tortuyaux@gmail.com>
2019-11-05 21:10:08 +0100

    api: add default configuration

--- api/main.go
+++ api/main.go
@@ -2,6 +2,33 @@ package api
 
 import "fmt"
 
+// Configuration is the global configuration of the API server
+type Configuration struct {
+	// Database configuration
+	Database ConfDB `json:"database"`
+	// Server configuration
+	Server ConfSrv `json:"server"`
+}
+
+// ConfDB is the database configuration
+type ConfDB struct {
+	// Username of the database
+	Username string `json:"username"`
+	// Password of the database
+	Password string `json:"password"`
+	// Host of the database
+	Host string `json:"host"`
+	// Port of the database
+	Port int `json:"port"`
+	// Name of the database to use
+	Name string `json:"name"`
+}
+
+type ConfSrv struct {
+	// Port to listen on
+	Port int `json:"port"`
+}
+
 func StartAPI(apiConfig string) error {
 	fmt.Println("config: ", apiConfig)
 	return nil
