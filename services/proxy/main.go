28a1c9713447d0677470602229447fe16e37ff85
Benoit GEORGELIN <benoit.georgelin@yulpa.io>
2019-11-07 20:04:41 -0500

    add SMTP TLS support

--- services/proxy/main.go
+++ services/proxy/main.go
@@ -2,8 +2,10 @@ package proxy
 
 import (
 	"bytes"
+	"crypto/tls"
 	"errors"
 	"fmt"
+	"net"
 	"io"
 	"io/ioutil"
 	"log"
@@ -23,7 +25,12 @@ type Configuration struct {
 	MaxRecipients int `yaml:"max_recipients"`
 	// YMAddr is the address of the YM entrypoint
 	YMAddr string `yaml:"yulmails_address"`
-	// TODO: add TLS configuration
+	// Enable tls : true/false
+	EnableTLS bool `yaml:"enable_tls"`
+	// TLS cert path X509
+	TLSCertPath string `yaml:"tls_cert_path"`
+	// TLS cert path X509
+	TLSKeyPath string `yaml:"tls_key_path"`	
 }
 
 type backend struct{ ymAddr string } //TODO: add API client here
@@ -33,14 +40,39 @@ type session struct {
 	ymAddr string
 }
 
+
 // Login will authenticate the user. You can use any authentication system here
 // `state` is a struct with IP addr, etc. So we can use the YM API service in order
 // to request environment / entity services about this IP
 func (b *backend) Login(state *smtp.ConnectionState, username, password string) (smtp.Session, error) {
-	if username != "username" || password != "password" {
+
+	// Reverse DNS lookup
+	var client_ReverseDns = "unknown"
+	
+	if reverse, _ := net.LookupAddr(state.RemoteAddr.String()); len(reverse) != 0 {
+		client_ReverseDns = reverse[0]
+	}
+
+	// client_ip, port, err
+	client_ip, _, _ := net.SplitHostPort(state.RemoteAddr.String())
+
+	// TODO: IP_REPUTATION (bd, stats, first time hold , blacklist, etc)
+	if client_ip == "172.18.0.1" {
+		return nil, errors.New("Sorry, can't talking to you. Please visit https://yulmails.io/dnsbl")
+	}
+	// Check Username / Password
+	if username != "username@domain.com" || password != "password" {
+
+		log.Println("client=",client_ReverseDns,"[", client_ip,"],sasl_method=PLAIN,sasl_username=",username,",failed: authentication failure")
+
 		return nil, errors.New("invalid credentials")
 	}
-	log.Println("successful authentication: ", state.RemoteAddr.String())
+
+
+
+	log.Println("client=",client_ReverseDns,"[", client_ip,"],sasl_method=PLAIN,sasl_username=",username)
+	
+	// Offer the SMTP SESSION
 	return &session{
 		to:     make([]string, 0),
 		ymAddr: b.ymAddr,
@@ -103,7 +135,25 @@ func StartProxy(proxyConf string) error {
 	s.Addr = fmt.Sprintf("%s:%d", c.Addr, c.Port)
 	s.MaxRecipients = c.MaxRecipients
 	s.AllowInsecureAuth = true
-	log.Println("starting proxy on: ", s.Addr)
+	
+	proxyCert, err := tls.LoadX509KeyPair(c.TLSCertPath, c.TLSKeyPath)
+
+    if err != nil {
+        log.Fatalf("Error: loadkeys: %s", err)
+    }
+
+    if c.EnableTLS {
+		s.TLSConfig = &tls.Config{
+						Certificates: []tls.Certificate{proxyCert},
+					}
+		log.Println("starting SMTP proxy with TLS_ENABLE on:", s.Addr)
+		log.Println("loading certificate: ", c.TLSCertPath)
+	
+	} else {
+	
+		log.Println("starting SMTP proxy (warning : no TLS) on:", s.Addr)
+	}
+	
 	if err := s.ListenAndServe(); err != nil {
 		return err
 	}
