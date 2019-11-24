776363d6032f05543d5a77be0f52a24d9d764a57
Mathieu Tortuyaux <mathieu.tortuyaux@gmail.com>
2019-11-24 13:33:08 +0100

    api: implement db connection and health check

--- api/main.go
+++ api/main.go
@@ -1,43 +1,47 @@
 package api
 
 import (
+	"database/sql"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"time"
 
-	"github.com/yulpa/yulmails/api/entity"
 	"github.com/yulpa/yulmails/api/abuse"
+	"github.com/yulpa/yulmails/api/entity"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-
-	_ "github.com/swaggo/http-swagger/example/go-chi/docs"
+	_ "github.com/lib/pq"
+	"gopkg.in/yaml.v2"
+	"github.com/pkg/errors"
 )
 
 // Configuration is the global configuration of the API server
 type Configuration struct {
 	// Database configuration
-	Database ConfDB `json:"database"`
+	Database ConfDB `yaml:"database"`
 	// Server configuration
-	Server ConfSrv `json:"server"`
+	Server ConfSrv `yaml:"server"`
 }
 
 // ConfDB is the database configuration
 type ConfDB struct {
 	// Username of the database
-	Username string `json:"username"`
+	Username string `yaml:"username"`
 	// Password of the database
-	Password string `json:"password"`
+	Password string `yaml:"password"`
 	// Host of the database
-	Host string `json:"host"`
+	Host string `yaml:"host"`
 	// Port of the database
-	Port int `json:"port"`
+	Port int `yaml:"port"`
 	// Name of the database to use
-	Name string `json:"name"`
+	Name string `yaml:"name"`
 }
 
 type ConfSrv struct {
 	// Port to listen on
-	Port int `json:"port"`
+	Port int `yaml:"port"`
 }
 
 // @title YulmailsAPI
@@ -54,6 +58,22 @@ type ConfSrv struct {
 // @host api-dev.yulmails.io
 // @BasePath /
 func StartAPI(apiConfig string) error {
+	content, err := ioutil.ReadFile(apiConfig)
+	if err != nil {
+		return errors.Wrap(err, "unable to open api config file")
+	}
+	var c Configuration
+	if err := yaml.Unmarshal(content, &c); err != nil {
+		return errors.Wrap(err, "unable to load configuration")
+	}
+	conn := fmt.Sprintf(
+		"user=%s dbname=%s sslmode=false password=%s host=%s port=%d",
+		c.Database.Username, c.Database.Name, c.Database.Password, c.Database.Host, c.Database.Port,
+	)
+	db, err := sql.Open("postgres", conn)
+	if err != nil {
+		return errors.Wrap(err, "unable to create db connection")
+	}
 	r := chi.NewRouter()
 
 	// A good base middleware stack
@@ -68,7 +88,12 @@ func StartAPI(apiConfig string) error {
 	r.Use(middleware.Timeout(60 * time.Second))
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hi"))
+		// health check on DB connection
+		if err := db.Ping(); err != nil {
+			w.WriteHeader(http.StatusServiceUnavailable)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
 	})
 
 	r.Route("/entities", func(r chi.Router) {
--- go.mod
+++ go.mod
@@ -19,9 +19,9 @@ require (
 	github.com/lib/pq v1.2.0
 	github.com/onsi/ginkgo v1.10.2 // indirect
 	github.com/onsi/gomega v1.7.0 // indirect
+	github.com/pkg/errors v0.8.1
 	github.com/sirupsen/logrus v1.4.2 // indirect
 	github.com/stretchr/testify v1.3.0
-	github.com/swaggo/http-swagger v0.0.0-20190614090009-c2865af9083e
 	github.com/swaggo/swag v1.6.3
 	github.com/urfave/cli v1.22.1
 	golang.org/x/net v0.0.0-20191011234655-491137f69257
